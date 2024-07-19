ec2361321740bec35ef8c15a7a57e5cbdc84b43f
endy-rkt <zk818019@gmail.com>
2024-07-19 14:12:16 +0300

    :sparkles: recursive mutex

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 09:52:07 by trazanad          #+#    #+#             */
-/*   Updated: 2024/07/18 16:33:06 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/07/19 14:10:09 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
+#include <string.h>
 #include <time.h>
 
-// chefs = threads
-// stove = shared data (+mutex)
+#define THREAD_NUM 8
 
-pthread_mutex_t stoveMutex[4];
-int stoveFuel[4] = { 100, 100, 100, 100 };
+pthread_mutex_t mutexFuel;
+int fuel = 50;
 
 void* routine(void* args) {
-    for (int i = 0; i < 4; i++) {
-        if (pthread_mutex_trylock(&stoveMutex[i]) == 0) {
-            int fuelNeeded = (rand() % 30);
-            if (stoveFuel[i] - fuelNeeded < 0) {
-                printf("No more fuel... going home\n");
-            } else {
-                stoveFuel[i] -= fuelNeeded;
-                usleep(500000);
-                printf("Fuel left %d\n", stoveFuel[i]);
-            }
-            pthread_mutex_unlock(&stoveMutex[i]);
-            break;
-        } else {
-            if (i == 3) {
-                printf("No stove available yet, waiting...\n");
-                usleep(300000);
-                i = 0;
-            }
-        }
-    }
+    pthread_mutex_lock(&mutexFuel); 
+    pthread_mutex_lock(&mutexFuel); 
+    pthread_mutex_lock(&mutexFuel); 
+    fuel += 50;
+    printf("Incremented fuel to: %d\n", fuel);
+    pthread_mutex_unlock(&mutexFuel);
+    pthread_mutex_unlock(&mutexFuel);
+    pthread_mutex_unlock(&mutexFuel);
 }
 
-int main(int argc, char* argv[]) {
-    srand(time(NULL));
-    pthread_t th[10];
-    for (int i = 0; i < 4; i++) {
-        pthread_mutex_init(&stoveMutex[i], NULL);
-    }
-    for (int i = 0; i < 10; i++) {
+int main(int argc, char *argv[]) {
+    pthread_t th[THREAD_NUM];
+	pthread_mutexattr_t	recursiveMutexAttribute;
+
+	pthread_mutexattr_init(&recursiveMutexAttribute);
+	pthread_mutexattr_settype(&recursiveMutexAttribute, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&mutexFuel, &recursiveMutexAttribute);
+    int i;
+    for (i = 0; i < THREAD_NUM; i++) {
         if (pthread_create(&th[i], NULL, &routine, NULL) != 0) {
             perror("Failed to create thread");
         }
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (i = 0; i < THREAD_NUM; i++) {
         if (pthread_join(th[i], NULL) != 0) {
             perror("Failed to join thread");
         }
     }
-    for (int i = 0; i < 4; i++) {
-        pthread_mutex_destroy(&stoveMutex[i]);
-    }
+    printf("Fuel: %d\n", fuel);
+	pthread_mutexattr_destroy(&recursiveMutexAttribute);
+    pthread_mutex_destroy(&mutexFuel);
     return 0;
 }
-
