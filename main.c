a5fe03e796aff730618fb034eb5c639f7c60a93d
endy-rkt <zk818019@gmail.com>
2024-07-20 14:33:57 +0300

    :sparkles: done learning thread and process

--- main.c
+++ main.c
@@ -6,54 +6,91 @@
 /*   By: trazanad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 09:52:07 by trazanad          #+#    #+#             */
-/*   Updated: 2024/07/19 14:10:09 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/07/20 14:32:03 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <time.h>
 
-#define THREAD_NUM 8
+#define THREAD_NUM 19
 
-pthread_mutex_t mutexFuel;
-int fuel = 50;
+typedef struct Task {
+    int a, b;
+} Task;
 
-void* routine(void* args) {
-    pthread_mutex_lock(&mutexFuel); 
-    pthread_mutex_lock(&mutexFuel); 
-    pthread_mutex_lock(&mutexFuel); 
-    fuel += 50;
-    printf("Incremented fuel to: %d\n", fuel);
-    pthread_mutex_unlock(&mutexFuel);
-    pthread_mutex_unlock(&mutexFuel);
-    pthread_mutex_unlock(&mutexFuel);
+Task taskQueue[256];
+int taskCount = 0;
+
+pthread_mutex_t mutexQueue;
+pthread_cond_t condQueue;
+
+void executeTask(Task* task) {
+    usleep(50000);
+    int result = task->a + task->b;
+    printf("The sum of %d and %d is %d\n", task->a, task->b, result);
 }
 
-int main(int argc, char *argv[]) {
-    pthread_t th[THREAD_NUM];
-	pthread_mutexattr_t	recursiveMutexAttribute;
+void submitTask(Task task) {
+    pthread_mutex_lock(&mutexQueue);
+    taskQueue[taskCount] = task;
+    taskCount++;
+    pthread_mutex_unlock(&mutexQueue);
+    pthread_cond_signal(&condQueue);
+}
 
-	pthread_mutexattr_init(&recursiveMutexAttribute);
-	pthread_mutexattr_settype(&recursiveMutexAttribute, PTHREAD_MUTEX_RECURSIVE);
-    pthread_mutex_init(&mutexFuel, &recursiveMutexAttribute);
+// 1 2 3 4 5
+// 2 3 4 5
+
+void* startThread(void* args) {
+    while (1) {
+        Task task;
+
+        pthread_mutex_lock(&mutexQueue);
+        while (taskCount == 0) {
+            pthread_cond_wait(&condQueue, &mutexQueue);
+        }
+
+        task = taskQueue[0];
+        int i;
+        for (i = 0; i < taskCount - 1; i++) {
+            taskQueue[i] = taskQueue[i + 1];
+        }
+        taskCount--;
+        pthread_mutex_unlock(&mutexQueue);
+        executeTask(&task);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t th[THREAD_NUM];
+    pthread_mutex_init(&mutexQueue, NULL);
+    pthread_cond_init(&condQueue, NULL);
     int i;
     for (i = 0; i < THREAD_NUM; i++) {
-        if (pthread_create(&th[i], NULL, &routine, NULL) != 0) {
-            perror("Failed to create thread");
+        if (pthread_create(&th[i], NULL, &startThread, NULL) != 0) {
+            perror("Failed to create the thread");
         }
+    }
+    srand(time(NULL));
+    for (i = 0; i < 100; i++) {
+        Task t = {
+            .a = rand() % 100,
+            .b = rand() % 100
+        };
+        submitTask(t);
     }
 
     for (i = 0; i < THREAD_NUM; i++) {
         if (pthread_join(th[i], NULL) != 0) {
-            perror("Failed to join thread");
+            perror("Failed to join the thread");
         }
     }
-    printf("Fuel: %d\n", fuel);
-	pthread_mutexattr_destroy(&recursiveMutexAttribute);
-    pthread_mutex_destroy(&mutexFuel);
+    pthread_mutex_destroy(&mutexQueue);
+    pthread_cond_destroy(&condQueue);
     return 0;
 }
