00ec80efdfb842380045979a9b1959b1c35a0892
endy-rkt <zk818019@gmail.com>
2024-08-14 16:45:17 +0300

    :sparkles: init word expansion (no double quote and spaghetti code)

--- lexer/expander.c
+++ lexer/expander.c
@@ -6,34 +6,146 @@
 /*   By: trazanad <trazanad@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 11:27:04 by trazanad          #+#    #+#             */
-/*   Updated: 2024/08/14 11:15:46 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/08/14 16:43:39 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-void	expand_word(t_token	**tk)
+char	*my_getenv(char *var)
 {
+	char	*str;
 
+	str = getenv(var);
+	if (!str)
+		return ("");
+	return (str);
 }
 
-// void	no_quote_delimiter(char *value, char *new_value, int i)
-// {
-// 	char	tmp;
+int	format_params(char *value, char **new_value, int i, int *j)
+{
+	int		k;
+	char	*tmp;
+	char	*param_value;
 
-// 	tmp = 0;
-// 	while (value[i])
-// 	{
-// 		if ((value[i] == '\'' || value[i] == '\"') && tmp == 0)
-// 			tmp = value[i];
-// 		else if (value[i] == tmp && tmp != 0)
-// 			tmp = 0;
-// 		else
-// 			new_value[i] = value[i];
-// 		i++;
-// 	}
-// 	new_value[i] = '\0';
-// }
+	if (ft_isspace(value[i + 1]) || value[i+1]==0 || value[i+1] == '\'' || value[i+1] == '\"')
+	{
+		*new_value = ft_strjoin(*new_value, "$");//0
+		*j = *j + 1;
+	}
+	i++;
+	if (!(ft_isalpha(value[i]) || value[i] == '_'))
+	{
+		if (!ft_isspace(value[i]) && value[i] && value[i] != '\'' && value[i] != '\"')
+			i++;
+		return (i);
+	}
+	tmp = malloc(ft_strlen(value) + 1);
+	k = 0;
+	while (ft_isalpha(value[i]) || value[i] == '_')
+	{
+		tmp[k] = value[i];
+		k++;
+		i++;
+	}
+	tmp[k] ='\0';
+	param_value = my_getenv(tmp);
+	*j = *j + ft_strlen(param_value);
+	*new_value = ft_strjoin(*new_value, param_value);//0
+	free(tmp);
+	return (i);
+}
+
+int	format_quoted(char *value, char **new_value, char c, int i)
+{
+	int		j;
+	int		end;
+
+	i++;
+	if (c == '\"')
+		while (value[i] != c)
+		{
+			if (value[i] == '$')
+				i = format_params(value, new_value, i, &j);
+			else
+			{
+		j = ft_strlen(*new_value);
+				*new_value[j] = value[i];
+				i++;
+			}
+		}
+	else
+	{
+		j = ft_strlen(*new_value);
+		while (value[i] != c)
+		{
+			*new_value[j] = value[i];
+			i++;
+			j++;
+		}		
+	}	
+	if (c == value[i])
+		i++;
+	return (i);
+}
+
+void	expand_word(t_token	**tk)
+{
+	int		i;
+	int		k;
+	int		j;
+	char 	*tmp;
+	char	*value;
+	char	*new_value;
+
+	i = 0;
+	value = (*tk)->value;
+	new_value = malloc(ft_strlen(value) + 1);
+	new_value[0] = '\0';
+	while (value[i])
+	{
+		printf("cond=%c\n", value[i]);
+		if (value[i] == '\'')
+		{
+			i++;
+			while (value[i] != '\'' && value[i])
+			{
+			k = 0;
+			tmp = malloc(ft_strlen(value) + 1);
+			tmp[0] = 0;
+			tmp[k] = value[i];
+			k++;
+			i++;
+			tmp[k] = 0;
+				j += ft_strlen(tmp);
+			new_value = ft_strjoin(new_value, tmp);//0
+			free(tmp);
+			}
+			if (value[i] == '\'')
+				i++;
+		}
+		else if (value[i] == '\"')
+			i = format_quoted(value, &new_value, value[i], i);
+		else if (value[i] == '$')
+			i = format_params(value, &new_value, i, &j);
+		else
+		{
+			k = 0;
+			tmp = malloc(ft_strlen(value) + 1);
+			tmp[0] = 0;
+			tmp[k] = value[i];
+			k++;
+			i++;
+			tmp[k] = 0;
+			j += ft_strlen(tmp);
+			new_value = ft_strjoin(new_value, tmp);//0
+			free(tmp);
+		}
+		printf("aft=%c\n", value[i]);
+	}
+	free(value);
+	(*tk)->value = new_value;
+}
 
 int	format_delimiter(char *value, char *new_value, int i, int j)
 {
@@ -73,7 +185,7 @@ void	expand_redir(t_token **tk)
 	while (value[i] == '0')
 		i++;
 	new_value = malloc(ft_strlen(value) + 1);
-	while ((ft_isdigit(value[i]) || value[i] == '>' || value[i] == '<') && value[i])
+	while ((ft_isdigit(value[i]) || ft_strchr("<>", value[i])) && value[i])
 	{
 		new_value[j] = value[i];
 		i++;
--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: trazanad <trazanad@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 08:40:52 by trazanad          #+#    #+#             */
-/*   Updated: 2024/08/02 11:10:18 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/08/14 14:22:47 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	len;
 
 	if (!s)
+		return (0);
+	if (s[0] == 0)
 		return (0);
 	len = 0;
 	while (s[len])
